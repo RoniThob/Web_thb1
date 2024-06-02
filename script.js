9f33bc2ee50f22deaab2bee91c42087170a78218
RoniThob <thobronidubois@gmail.com>
2024-06-02 20:21:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,41 +18,6 @@ window.onscroll = () => {
         }
     })
 }
-let progres = document.getElementById("progres");
-            let song = document.getElementById("song");
-            let ctrlIcon = document.getElementById("progres");
-
-            song.onloadedmetadata = function(){
-                progres.max = song.duration;
-                progres.value = song.currentTime;
-            }
-
-            function playPause(){
-                if(ctrlIcon.classList.contains("fa-pause")){
-                    song.pause();
-                    ctrlIcon.classList.remove("fa-pause");
-                    ctrlIcon.classList.add("fa-play");
-                }
-                else{
-                    song.play();
-                    ctrlIcon.classList.add("fa-pause");
-                    ctrlIcon.classList.remove("fa-play");
-                }
-            }
-
-            if(song.play()){
-                setInterval(()=>{
-                    progres.value = song.currentTime
-                },500);
-            }
-            progres.onchange = function(){
-                song.play();
-                song.currentTime = progres.value;
-                ctrlIcon.classList.add("fa-pause");
-                ctrlIcon.classList.remove("fa-play");
-            }
-
-
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
